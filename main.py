04a0bb636a63a4c55286b53ac16122dcd00d5cd4
KASH33KASH <111311665+KASH33KASH@users.noreply.github.com>
2023-02-20 15:56:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import pyautogui
 import datetime
 import threading
 
-token = "5462614670:AAHVubo_KmPvnOzNcKuN67CmhFxRtLDJ1O4"
+token = "token"
 lock = False
 
 telegram_bot = telebot.TeleBot(token)
